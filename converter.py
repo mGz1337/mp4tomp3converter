a1c6926680b31490e14b515d48b169c00a8197b3
mGz1337 <56359866+mGz1337@users.noreply.github.com>
2021-02-23 16:13:04 +0200

    Add files via upload
    quality of life update

---
+++ converter.py
@@ -0,0 +1,19 @@
+from moviepy.editor import *
+try :
+    video = input("What is the name of your file? (include mp4) >> ")
+
+    mp4_file = video
+    mp3_file = "{}.mp3".format(mp4_file)
+    videoClip = VideoFileClip(mp4_file)
+    audioclip = videoClip.audio
+    audioclip.write_audiofile(mp3_file)
+except NameError:
+    print("")
+    exit("You either didn't install the moviepy library, or you didn't put the correct name! Be sure to include mp4")
+except OSError:
+    print("")
+    exit("You either didn't install the moviepy library, or you didn't put the correct name! Be sure to include .mp4 after the song name!")
+audioclip.close()
+videoClip.close()
+print("")
+exit("The mp4 file has been successfully converted to mp3. Enjoy! :)")
