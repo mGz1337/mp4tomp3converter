b5a32892e471fff46f05f8dfd11d42fb85283411
mGz1337 <56359866+mGz1337@users.noreply.github.com>
2021-02-23 16:25:53 +0200

    Update converter.py

--- converter.py
+++ converter.py
@@ -6,7 +6,6 @@
 from moviepy.editor import *
 try :
     video = input("What is the name of your file? (include mp4) >> ")
-
     mp4_file = video
     mp3_file = "{}.mp3".format(mp4_file)
     videoClip = VideoFileClip(mp4_file)
